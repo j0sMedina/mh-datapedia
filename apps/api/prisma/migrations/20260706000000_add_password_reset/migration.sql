49fe2dba483220f0df0224695b1c7a2d40894d26
j0sMedina <jos.mathiasmg@gmail.com>
2026-07-06 15:05:45 -0600

    feat(api): add resetPasswordToken fields to User schema

---
+++ apps/api/prisma/migrations/20260706000000_add_password_reset/migration.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "User" ADD COLUMN "resetPasswordToken" TEXT;
+ALTER TABLE "User" ADD COLUMN "resetPasswordTokenExpiry" TIMESTAMP(3);
+CREATE UNIQUE INDEX "User_resetPasswordToken_key" ON "User"("resetPasswordToken");
